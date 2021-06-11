143caee72e741ac74c2c700f2281dda09f37dd38
T. Allen Harding <softwareisharding@gmail.com>
2021-06-11 12:57:02 -0700

    server calls ip-api.com for new client info

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const request = require('request');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,13 +15,19 @@ const $ = require('jquery')(window);
 
 const PORT = process.env.PORT || 29070;
 
-//app.use(express.static(path.join(__dirname, 'public')));
-app.get("/", function(request, response) {
-    response.send(request.header('x-forwarded-for') || request.socket.remoteAddress);
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
+
+request('http://ip-api.com/json', function(err, res){
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log(res.body);
+    }
+});
 
 io.on('connection', socket => {
     socket.on('user-enter', username => {
