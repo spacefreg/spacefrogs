4f3071b37c090571fad9e9b48c7c4324848d9ca2
T. Allen Harding <softwareisharding@gmail.com>
2021-07-28 23:31:13 -0700

    first mongodb deploy

--- server.js
+++ server.js
@@ -2,24 +2,49 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 //const socketIO = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-
-//const io = socketIO(server);
-
+const mongoose = require('mongoose');
 
 //jquery
 const { JSDOM } = require('jsdom');
 const { window } = new JSDOM('');
 const $ = require('jquery')(window);
 
+const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT || 29070;
+
+//const io = socketIO(server);
+const uri = "mongodb+srv://freg:test123@nrol-39.2degb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connection open with mongodb');
+    })
+    .catch(err => {
+        console.log('oh no didnt connect!');
+    })
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+
+const testSchema = mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Anon = mongoose.model('Anon', testSchema);
+
+Anon.insertMany([
+    { name: 'Freg', age: 24 },
+    { name: 'Bozay', age: 25} ,
+    { name: 'cowboy', age: 1337 }
+])
+    .then(data => {
+        console.log('insertMany successful');
+        console.log(data);
+    });
 
 
 //mothballing socket.io until some kind of multiplayer is added (7/25/21)
