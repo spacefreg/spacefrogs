8d207ba4d4e8580c90b66efe2eb129cf5e94d5d7
T. Allen Harding <softwareisharding@gmail.com>
2022-01-30 00:12:23 -0800

    disabled mobile zooming and scrolling

--- public/src/client.js
+++ public/src/client.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('canv');
 const ctx = canvas.getContext('2d');
 
+document.documentElement.requestFullscreen();
+
 canvas.setAttribute('width', '400');
 canvas.setAttribute('height', '800');
 
@@ -10,3 +12,15 @@ ctx.fillStyle = 'red';
 ctx.fill();
 
 console.log('hello frfom client.js');
+
+
+document.addEventListener('click', function(e) {
+    console.log('click');
+    if (e.target.nodeName !== 'INPUT') {
+        e.preventDefault();
+    }
+});
+
+document.body.classList.add('stop-scrolling');
+
+
