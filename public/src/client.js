118d1b5b5d355c43c7af0b078306a71f8e9df145
T. Allen Harding <softwareisharding@gmail.com>
2022-02-04 13:44:41 -0800

    canvas can now be drawn on + cleared

--- public/src/client.js
+++ public/src/client.js
@@ -1,10 +1,9 @@
 const canvas = document.getElementById('canv');
 const ctx = canvas.getContext('2d');
 
-document.documentElement.requestFullscreen();
+let isDrawing = false;
 
-//canvas.setAttribute('width', '400');
-//canvas.setAttribute('height', '800');
+
 
 const canvWidth = canvas.width;
 const canvHeight = canvas.height;
@@ -18,7 +17,6 @@ console.log('hello from client.js');
 
 
 document.addEventListener('click', function(e) {
-    console.log('click');
     if (e.target.nodeName !== 'INPUT') {
         e.preventDefault();
     }
@@ -26,4 +24,60 @@ document.addEventListener('click', function(e) {
 
 document.body.classList.add('stop-scrolling');
 
+
+
+canvas.addEventListener('mousedown', function(e) {
+    console.log(e.pageX - canvas.offsetLeft);
+    console.log(e.pageY - canvas.offsetTop);
+});
+
+
+function beginDraw(evt) {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(evt.pageX - canvas.offsetLeft, evt.pageY - canvas.offsetTop);
+}
+
+function continueDraw(evt) {
+    if (!isDrawing) {
+        return;
+    } 
+    ctx.lineTo(evt.pageX - canvas.offsetLeft, evt.pageY - canvas.offsetTop);
+    ctx.stroke();
+}
+
+function endDraw(evt) {
+    if (!isDrawing) {
+        return;
+    }
+    continueDraw(evt);
+    isDrawing = false;
+}
+
+
+
+function beginTouch(evt) {
+    beginDraw(evt.touches[0]);
+}
+
+function touchMove(evt) {
+    continueDraw(evt.touches[0]);
+    evt.preventDefault();
+}
+
+function touchEnd(evt) {
+    endDraw(evt.changedTouches[0]);
+}
+
+canvas.addEventListener('touchstart', beginTouch, false);
+canvas.addEventListener('touchmove', touchMove, false);
+canvas.addEventListener('touchend', touchEnd, false);
+
+
+
+const clearBtn = document.getElementById('clear-btn');
+clearBtn.onclick = function() {
+    ctx.fillStyle = 'rgb(175, 203, 204)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 
