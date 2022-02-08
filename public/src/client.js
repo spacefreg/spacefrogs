4bfc82760d024772c692ab3f20c5657ff3c6b1bf
T. Allen Harding <softwareisharding@gmail.com>
2022-02-08 13:45:26 -0800

    Revert "migrated to typescript, early stages"
    This reverts commit a78490eb5184abfd3d2bca18e15f7cbec5c61244.

--- public/src/client.js
+++ public/src/client.js
@@ -1,28 +1,41 @@
-var canvas = document.getElementById('canv');
-var ctx = canvas.getContext('2d');
-var isDrawing = false;
+const canvas = document.getElementById('canv');
+const ctx = canvas.getContext('2d');
+
+let isDrawing = false;
+
+
 ctx.beginPath();
-ctx.rect(0, 0, canvas.width, canvas.height);
-ctx.fillStyle = "rgb(175, 203, 204)";
+ctx.rect(0, 0, canvWidth, canvHeight);
+ctx.fillStyle = `rgb(175, 203, 204)`;
 ctx.fill();
+
 ctx.lineWidth = 5;
-console.log('hello from client.js');
-document.addEventListener('click', function (e) {
-    e.preventDefault();
+
+
+document.addEventListener('click', function(e) {
+    if (e.target.nodeName !== 'INPUT') {
+        e.preventDefault();
+    }
 });
+
 document.body.classList.add('stop-scrolling');
+
+
+
 function beginDraw(evt) {
     isDrawing = true;
     ctx.beginPath();
     ctx.moveTo(evt.pageX - canvas.offsetLeft, evt.pageY - canvas.offsetTop);
 }
+
 function continueDraw(evt) {
     if (!isDrawing) {
         return;
-    }
+    } 
     ctx.lineTo(evt.pageX - canvas.offsetLeft, evt.pageY - canvas.offsetTop);
     ctx.stroke();
 }
+
 function endDraw(evt) {
     if (!isDrawing) {
         return;
@@ -30,21 +43,31 @@ function endDraw(evt) {
     continueDraw(evt);
     isDrawing = false;
 }
+
+
+
 function beginTouch(evt) {
     beginDraw(evt.touches[0]);
 }
+
 function touchMove(evt) {
     continueDraw(evt.touches[0]);
     evt.preventDefault();
 }
+
 function touchEnd(evt) {
     endDraw(evt.changedTouches[0]);
 }
+
 canvas.addEventListener('touchstart', beginTouch, false);
 canvas.addEventListener('touchmove', touchMove, false);
 canvas.addEventListener('touchend', touchEnd, false);
-var clearBtn = document.getElementById('clear-btn');
-clearBtn.onclick = function () {
+
+
+
+const clearBtn = document.getElementById('clear-btn');
+clearBtn.onclick = function() {
     ctx.fillStyle = 'rgb(175, 203, 204)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
+}
+
