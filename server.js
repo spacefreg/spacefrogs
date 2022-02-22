2629318056d6ef36c39b7417c143a5bf20427821
T. Allen Harding <softwareisharding@gmail.com>
2022-02-21 19:00:53 -0800

    testing deploy of SFML executable download

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ const PORT = process.env.PORT || 29070;
 server.listen(PORT, () => console.log(`server listening on port ${PORT}`));
 
 server.use(express.static(path.join(__dirname, 'public')));
+
+server.get('/download', (req, res) => {
+    const file = __dirname + '/public/res/helios-client.zip';
+    res.download(file);
+});
