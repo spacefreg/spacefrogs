b6851c45d1e0aea7d8fb4499029d22c7b2b061d9
T. Allen Harding <softwareisharding@gmail.com>
2022-02-27 21:11:14 -0800

    es module import

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const server = express();
 const PORT = process.env.PORT || 29070;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 server.listen(PORT, () => console.log(`server listening on port ${PORT}`));
 
 server.use(express.static(path.join(__dirname, 'public')));
-
-server.get('/download', (req, res) => {
-    const file = __dirname + '/public/res/helios-client.zip';
-    res.download(file);
-});
