c8c3cb5b3910582e73e4ae15536b21564458b2a7
spacefreg <fregspaceman@gmail.com>
2022-04-05 15:25:42 -0700

    the sun is now 'properly' centered in the solar system

--- src/public/core/planets/sun.ts
+++ src/public/core/planets/sun.ts
@@ -13,11 +13,13 @@ export default class Sun extends Planet {
     }
 
     public update(dt: number) {
-        if (!this.initialized) {
-            //(4/5/22) this is just my current (awful) way of initiliazing the sun's position after its image has already loaded
-            const solarOrigin: vec2 = new vec2(this.planetElement.getOrigin().x, this.planetElement.getOrigin().y);
-            this.planetElement.setOrigin(new vec2(solarOrigin.x - 52, solarOrigin.y - 52));
-            this.initialized = true;
+        //(4/5/22) bootlet sun.init() function at the beginning of update. this.initialized flag just keeps track of if we have already centered the sun or not. this cna't be done in the constuctor because the sun's image is not yet loaded
+        if (!this.initialized && this.planetElement.getImageSize().x > 0) {
+            if (this.planetElement.getImageSize().x > 0) {
+                const oldOrigin: vec2 = new vec2(this.planetElement.getOrigin().x, this.planetElement.getOrigin().y);
+                this.planetElement.setOrigin(new vec2(oldOrigin.x - (this.planetElement.getImageSize().x / 2), oldOrigin.y - (this.planetElement.getImageSize().y / 2)));
+                this.initialized = true;
+            }
         }
 
         this.planetElement.update(dt);
